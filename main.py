aa15ba697bebd1abc0f8ea2f816f1d5a0275d0e7
ibaragozz <baragozz1@gmail.com>
2024-11-25 20:43:23 +0300

    HW37done

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-# def add(a,b):
-#     return a+b
-#
-# def sub(a,b):
-#     return a-b
-#
-# def mul(a,b):
-#     return a*b
-#
-# def div(a,b):
-#     return a/b
+def add(a,b):
+    return a+b
 
-# def check(number):
-#     return number % 2 == 0
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def check(number):
+    return number % 2 == 0
 
 def divide(a, b):
     if b == 0:
         raise ValueError('Cannot divide by zero')
     return a / b
+
+# Домашнее задание
+
+def hw(a, b):
+    if b == 0:
+        raise ValueError('Cannot divide by zero')
+    return a % b
--- test.py
+++ test.py
@@ -1,34 +1,35 @@
 import unittest
 from main import divide
-# from main import check
-# from main import add, sub, mul, div
+from main import check
+from main import add, sub, mul, div
+from main import hw
 
-# class TestMath(unittest.TestCase):
-#     def test_add(self):
-#         self.assertEqual(add(10, 5), 15)
-#         self.assertNotEqual(add(3, 7), 9)
-#
-#     def test_sub(self):
-#         self.assertEqual(sub(10, 5), 5)
-#         self.assertNotEqual(sub(3, 7), 4)
-#
-#     def test_mul(self):
-#         self.assertEqual(mul(10, 5), 50)
-#         self.assertNotEqual(mul(3, 7), 20)
-#
-#     def test_div(self):
-#         self.assertEqual(div(10, 5), 2)
-#         self.assertNotEqual(div(21, 7), 5)
+class TestMath(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(10, 5), 15)
+        self.assertNotEqual(add(3, 7), 9)
 
-# class TestCheck(unittest.TestCase):
-#     def test_check(self):
-#         self.assertTrue(check(10))
-#         self.assertTrue(check(6))
-#         self.assertTrue(check(258))
-#
-#         self.assertFalse(check(21))
-#         self.assertTrue(not check(9))
-#         self.assertTrue(not check(1))
+    def test_sub(self):
+        self.assertEqual(sub(10, 5), 5)
+        self.assertNotEqual(sub(3, 7), 4)
+
+    def test_mul(self):
+        self.assertEqual(mul(10, 5), 50)
+        self.assertNotEqual(mul(3, 7), 20)
+
+    def test_div(self):
+        self.assertEqual(div(10, 5), 2)
+        self.assertNotEqual(div(21, 7), 5)
+
+class TestCheck(unittest.TestCase):
+    def test_check(self):
+        self.assertTrue(check(10))
+        self.assertTrue(check(6))
+        self.assertTrue(check(258))
+
+        self.assertFalse(check(21))
+        self.assertTrue(not check(9))
+        self.assertTrue(not check(1))
 class TestDivide(unittest.TestCase):
     def test_divide_success(self):
         self.assertEqual(divide(10, 5), 2)
@@ -36,6 +37,16 @@ class TestDivide(unittest.TestCase):
 
     def test_divide_by_zero(self):
         self.assertRaises(ValueError, divide, 10, 0)
+
+# Домашнее задание
+class TestHW(unittest.TestCase):
+    def test_hw_success(self):
+        self.assertEqual(hw(100, 5), 0)
+        self.assertEqual(hw(200, 5), 0)
+        self.assertNotEqual(hw(21, 7), 5)
+
+    def test_hw_by_zero(self):
+        self.assertRaises(ValueError, hw, 10, 0)
 
 
 if __name__ == '__main__':
